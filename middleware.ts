436f55253d27acd7ff7370486e423215cf9a4855
Saurabh <saurabh0346@gmail.com>
2024-06-08 19:21:53 +0530

    commit

--- middleware.ts
+++ middleware.ts
@@ -7,4 +7,3 @@ export default authMiddleware({
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-// matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
