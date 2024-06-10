37e02d7ae2aead2753b62a78885f3109e6906b3d
Saurabh <saurabh0346@gmail.com>
2024-06-10 12:21:14 +0530

    signinchange

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,5 @@
-import { SignIn } from "@clerk/nextjs";
+// import { SignIn } from "@clerk/nextjs";
+import { SignIn } from "../clerk/nextjs";
 
 const SignInPage = () => {
   return <SignIn />;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,5 @@
-import { SignUp } from "@clerk/nextjs";
+// import { SignUp } from "@clerk/nextjs";\
+import { SignUp } from "../clerk/nextjs";
 
 const SignUpPage = () => {
   return <SignUp />;
